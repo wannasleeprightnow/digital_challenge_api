44628ef5ab47b9b9e51da7c6a8ca8468329153d8
wannasleeprightnow <wannasleeprightnowww@gmail.com>
2023-11-18 22:00:54 +0500

    fix socket

--- src/main.py
+++ src/main.py
@@ -10,7 +10,8 @@ from db.database import create_tables
 app = FastAPI()
 main_router = APIRouter(prefix="/api/v1")
 
-origins = ["http://localhost:8080"]
+origins = ["http://localhost:8080",
+           "ws://localhost:8080"]
 
 app.add_middleware(
     CORSMiddleware,
